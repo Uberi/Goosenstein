a1c246c43423a0cb41f4079ef82e09fc9b83e5b6
Uberi <azhang9@gmail.com>
2014-03-28 22:53:22 -0400

    More update (press w!).

--- levels.lua
+++ levels.lua
@@ -1,7 +1,10 @@
-levels = levels or {}
+level = level or {}
 
-function levels:initialize()
-	love.physics.setMeter(80) --80 pixels to the meter
-	
+love.physics.setMeter(80) --80 pixels to the meter
+
+function level:create()
+	self = {}
+	self.world = love.physics.newWorld(0, 9.81 * 80, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
+	self.physical_objects = {}
+	return self
 end
-
--- scenes/intro.lua
+++ scenes/intro.lua
@@ -5,9 +5,11 @@ require "levels"
 scenes.intro = {
 	initialize = function(self)
 		self.bloom_effect = bloom:create()
+		self.bloom_effect.radius = 4
+		self.bloom_effect.samples = 4
 
 		self.pixel_small = love.graphics.newFont("fonts/Victor Pixel.ttf", 36)
-		self.logo = love.graphics.newImage("images/Goosenstein.png")
+		self.background = love.graphics.newImage("images/Background Morning.png")
 
 		self.pause_play_button = button:create("ii", 10, 10, 40, 40, self.pixel_small)
 		
@@ -17,15 +19,17 @@ scenes.intro = {
 		rain:setParticleLifetime(16, 20) --each particle lives for 5 seconds
 		rain:setPosition(width / 2, 0) --emit particles from center of left edge
 		rain:setDirection(80 * math.pi / 180) --emit downwards
-		rain:setSpread(5 * math.pi / 180)
+		rain:setRotation(-10 * math.pi / 180) --slant the raindrops
 		rain:setSpeed(460, 500)
-		rain:setSizes(0.8, 0.6, 0)
+		rain:setSizes(1.2, 0.8, 0)
 		rain:setSizeVariation(0.8)
 		rain:setAreaSpread("uniform", width / 2, 0)
 		rain:start()
 		self.rain = rain
 
 		for i = 1, 200 do self.rain:update(0.05) end --ensure the particle system is in a good initial state
+		
+		self.camera_x, self.camera_y = 0, 0
 	end,
 	mousereleased = function(self, x, y, button)
 		self.pause_play_button:mousereleased(x, y, button)
@@ -42,11 +46,14 @@ scenes.intro = {
 			else
 				self.pause_play_button.text = ">"
 				return --make sure the game is paused
-				--wip: parallax background
-				--wip: rain effect
 			end
 		end
 		
+		if love.keyboard.isDown("w") then
+			self.camera_x = self.camera_x - 80 * dt
+		end
+		
+		--fade in rectangle alpha
 		if elapsed <= 0.5 then
 			self.darkening = 255
 		elseif elapsed <= 1 then
@@ -60,6 +67,20 @@ scenes.intro = {
 		love.graphics.setColor(255, 255, 255)
 		
 		self.bloom_effect:before_draw()
+		
+		love.graphics.push()
+		love.graphics.translate(self.camera_x % self.background:getWidth(), 0)
+		love.graphics.draw(self.background, -self.background:getWidth(), 0, 0, height / self.background:getHeight())
+		love.graphics.draw(self.background, 0, 0, 0, height / self.background:getHeight())
+		love.graphics.draw(self.background, self.background:getWidth(), 0, 0, height / self.background:getHeight())
+		love.graphics.pop()
+		
+		love.graphics.push()
+		love.graphics.translate(self.camera_x, self.camera_y)
+		for i = 0, 10 do
+			love.graphics.rectangle("fill", 100 + i * 250, 100, 200, 200)
+		end
+		love.graphics.pop()
 		
 		love.graphics.draw(self.rain, 0, 0)
 		
