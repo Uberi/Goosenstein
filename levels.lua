993c7af8c2480a239c05f1ed8563a97d4933e647
Uberi <azhang9@gmail.com>
2014-03-29 07:07:55 -0400

    More updates.

---
+++ levels.lua
@@ -0,0 +1,86 @@
+function init_rain()
+	local rain = love.graphics.newParticleSystem(love.graphics.newImage("images/rain.png"), 2000)
+		rain:setEmissionRate(80) --emitted particles per second
+		rain:setEmitterLifetime(-1) --continuously emit particles
+		rain:setParticleLifetime(16, 20) --each particle lives for 5 seconds
+		rain:setPosition(width / 2, 0) --emit particles from center of left edge
+		rain:setDirection(80 * math.pi / 180) --emit downwards
+		rain:setRotation(-10 * math.pi / 180) --slant the raindrops
+		rain:setSpeed(460, 500)
+		rain:setSizes(1.2, 0.8, 0)
+		rain:setSizeVariation(0.8)
+		rain:setAreaSpread("uniform", width / 2, 0)
+		rain:start()
+
+		for i = 1, 200 do rain:update(0.05) end --ensure the particle system is in a good initial state
+		return rain
+end
+
+function init_character(collider)
+	local character = {
+		x = 500, y = 0,
+		w = 80, h = 100,
+		image = love.graphics.newImage("images/Character.png"),
+		scale = 0.4,
+		velocity_x = 0, velocity_y = 0,
+		mtv_x = nil, mtv_y = nil,
+		flipped = false,
+	}
+	character.shape = collider:addRectangle(character.x, character.y, character.w, character.h)
+	character.quads = {}
+	for i = 1, 8 do
+		table.insert(character.quads, love.graphics.newQuad(0, (i - 1) * 250, 200, 250, character.image:getWidth(), character.image:getHeight()))
+	end
+	
+	collider:setCallbacks(function(dt, shape_s, shape_t, dx, dy)
+		if shape_s == character.shape or shape_t == character.shape then --keep it out of collision
+			character.mtv_x, character.mtv_y = dx, dy
+		end
+	end)
+	return character
+end
+
+function init_map(map, collider, tile_size)
+	for i, row in ipairs(map) do
+		for j, value in ipairs(row) do
+			if value ~= 0 then
+				local shape = collider:addRectangle((j - 1) * tile_size, (i - 1) * tile_size, tile_size, tile_size)
+				collider:addToGroup("tiles", shape)
+				collider:setPassive(shape)
+			end
+		end
+	end
+	local tiles = love.graphics.newImage("images/Tiles.png")
+	local tile_quads = {}
+	for i = 1, 6 do
+		local rect = love.graphics.newQuad(0, (i - 1) * 128, 128, 128, tiles:getWidth(), tiles:getHeight())
+		table.insert(tile_quads, rect)
+	end
+	return tiles, tile_quads
+end
+
+function draw_map(map, tiles, tile_quads, tile_size, camera_x, camera_y)
+	--draw map
+	love.graphics.push()
+	love.graphics.translate(-camera_x, -camera_y)
+	for i, row in ipairs(map) do
+		for j, value in ipairs(row) do
+			if value ~= 0 then
+				love.graphics.draw(tiles, tile_quads[value], (j - 1) * tile_size, (i - 1) * tile_size, 0, tile_size / 128)
+			end
+		end
+	end
+	love.graphics.pop()
+end
+
+function draw_character(character, camera_x, camera_y)
+	--draw the character
+	love.graphics.push()
+	love.graphics.translate(-camera_x, -camera_y)
+	if character.flipped then
+		love.graphics.draw(character.image, character.state, character.x + character.w, character.y + 7, 0, -character.scale, character.scale)
+	else
+		love.graphics.draw(character.image, character.state, character.x, character.y + 7, 0, character.scale)
+	end
+	love.graphics.pop()
+end
--- scenes/intro.lua
+++ scenes/intro.lua
@@ -3,24 +3,7 @@ scenes = scenes or {}
 require "utilities/bloom"
 require "utilities/buttons"
 HC = require "hardoncollider"
-
-function init_rain()
-	local rain = love.graphics.newParticleSystem(love.graphics.newImage("images/rain.png"), 2000)
-		rain:setEmissionRate(80) --emitted particles per second
-		rain:setEmitterLifetime(-1) --continuously emit particles
-		rain:setParticleLifetime(16, 20) --each particle lives for 5 seconds
-		rain:setPosition(width / 2, 0) --emit particles from center of left edge
-		rain:setDirection(80 * math.pi / 180) --emit downwards
-		rain:setRotation(-10 * math.pi / 180) --slant the raindrops
-		rain:setSpeed(460, 500)
-		rain:setSizes(1.2, 0.8, 0)
-		rain:setSizeVariation(0.8)
-		rain:setAreaSpread("uniform", width / 2, 0)
-		rain:start()
-
-		for i = 1, 200 do rain:update(0.05) end --ensure the particle system is in a good initial state
-		return rain
-end
+require "levels"
 
 scenes.intro = {
 	initialize = function(self)
@@ -30,6 +13,7 @@ scenes.intro = {
 
 		self.pixel_small = love.graphics.newFont("fonts/Victor Pixel.ttf", 36)
 		self.background = love.graphics.newImage("images/Background Morning.png")
+		self.goose_flying = love.graphics.newImage("images/Goose Flying.png")
 
 		self.pause_play_button = button:create("ii", 10, 10, 40, 40, self.pixel_small)
 		
@@ -41,27 +25,10 @@ scenes.intro = {
 		self.world = love.physics.newWorld(0, 9.81 * 10, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
 		self.collider = HC(100)
 		
-		self.character = {
-			x = 500, y = 0,
-			w = 80, h = 100,
-			image = love.graphics.newImage("images/Character.png"),
-			scale = 0.4,
-			velocity_x = 0, velocity_y = 0,
-			mtv_x = nil, mtv_y = nil,
-			flipped = false,
-		}
-		self.character.shape = self.collider:addRectangle(self.character.x, self.character.y, self.character.w, self.character.h)
-		self.character.idle_quads = {}
-		for i = 1, 4 do
-			table.insert(self.character.idle_quads, love.graphics.newQuad(0, (i - 1) * 250, 200, 250, self.character.image:getWidth(), self.character.image:getHeight()))
-		end
+		self.character = init_character(self.collider)
 		
-		self.collider:setCallbacks(function(dt, shape_s, shape_t, dx, dy)
-			if shape_s == self.character.shape or shape_t == self.character.shape then --keep it out of collision
-				self.character.mtv_x, self.character.mtv_y = dx, dy
-			end
-		end)
 		
+		self.tile_size = 50
 		self.map = {
 			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -79,23 +46,7 @@ scenes.intro = {
 			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		}
-		
-		self.tile_size = 50
-		for i, row in ipairs(self.map) do
-			for j, value in ipairs(row) do
-				if value ~= 0 then
-					local shape = self.collider:addRectangle((j - 1) * self.tile_size, (i - 1) * self.tile_size, self.tile_size, self.tile_size)
-					self.collider:addToGroup("tiles", shape)
-					self.collider:setPassive(shape)
-				end
-			end
-		end
-		self.tiles = love.graphics.newImage("images/Tiles.png")
-		self.tile_quads = {}
-		for i = 1, 6 do
-			local rect = love.graphics.newQuad(0, (i - 1) * 128, 128, 128, self.tiles:getWidth(), self.tiles:getHeight())
-			table.insert(self.tile_quads, rect)
-		end
+		self.tiles, self.tile_quads = init_map(self.map, self.collider, self.tile_size)
 	end,
 	mousereleased = function(self, x, y, button)
 		self.pause_play_button:mousereleased(x, y, button)
@@ -124,15 +75,23 @@ scenes.intro = {
 		end
 		
 		local move_speed = 300
+		local actuated = false
 		if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
 			self.character.x = self.character.x - move_speed * dt
 			self.character.flipped = true
-			speedx = -move_speed
+			actuated = true
 		end
 		if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
 			self.character.x = self.character.x + move_speed * dt
 			self.character.flipped = false
+			actuated = true
 		end
+		if actuated then
+			self.character.state = self.character.quads[(math.floor(elapsed / 0.1) % 4) + 5]
+		else
+			self.character.state = self.character.quads[(math.floor(elapsed / 0.3) % 4) + 1]
+		end
+
 		self.character.velocity_y = self.character.velocity_y + 600 * dt
 		self.character.x = self.character.x + self.character.velocity_x * dt
 		self.character.y = self.character.y + self.character.velocity_y * dt
@@ -171,33 +130,9 @@ scenes.intro = {
 		love.graphics.draw(self.background, self.background:getWidth(), 0, 0, height / self.background:getHeight())
 		love.graphics.pop()
 		
-		--draw the character
-		love.graphics.push()
-		love.graphics.translate(-self.camera_x, -self.camera_y)
-		local index = (math.floor(elapsed / 0.3) % 4) + 1
-		if self.character.flipped then
-			love.graphics.draw(self.character.image, self.character.idle_quads[index], self.character.x + self.character.w, self.character.y + 7, 0, -self.character.scale, self.character.scale)
-		else
-			love.graphics.draw(self.character.image, self.character.idle_quads[index], self.character.x, self.character.y + 7, 0, self.character.scale)
-		end
-		love.graphics.pop()
-		
+		draw_character(self.character, self.camera_x, self.camera_y)
 		love.graphics.draw(self.rain, 0, 0)
-		
-		--draw map
-		love.graphics.push()
-		love.graphics.translate(-self.camera_x, -self.camera_y)
-		local count_x, count_y = math.ceil(width / self.tile_size), math.ceil(height / self.tile_size)
-		local index_x, index_y = math.floor(self.camera_x / self.tile_size), math.floor(self.camera_y / self.tile_size)
-		for i, row in ipairs(self.map) do
-			for j, value in ipairs(row) do
-				if value ~= 0 then
-					love.graphics.draw(self.tiles, self.tile_quads[value], (j - 1) * self.tile_size, (i - 1) * self.tile_size, 0, self.tile_size / 128)
-				end
-			end
-		end
-		love.graphics.pop()
-		
+		draw_map(self.map, self.tiles, self.tile_quads, self.tile_size, self.camera_x, self.camera_y)
 		self.pause_play_button:draw()
 		
 		self.bloom_effect:after_draw()
